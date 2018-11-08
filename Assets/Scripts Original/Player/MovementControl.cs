8a9301ef25adb478f4a2669db940c4f620de3dfd
Selteth <selteth@gmail.com>
2018-11-08 14:15:38 +0200

    Add horizntal movement

---
+++ Assets/Scripts Original/Player/MovementControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+    public float moveForce = 365f;          // Amount of force added to move the player left and right.
+    public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+
+    /* Components */
+    private Rigidbody2D rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        MoveHorizontal(horizontalAxis);
+    }
+
+    void MoveHorizontal(float horizontalAxis)
+    {
+        /* Accelerate the speed */
+        if (horizontalAxis * rigidbody.velocity.x < maxSpeed)
+            rigidbody.AddForce(Vector2.right * moveForce * horizontalAxis);
+
+        /* Limit player maximum speed */
+        if (Mathf.Abs(rigidbody.velocity.x) > maxSpeed)
+            rigidbody.velocity = new Vector2(Mathf.Sign(rigidbody.velocity.x) * maxSpeed, rigidbody.velocity.y);
+    }
+}
