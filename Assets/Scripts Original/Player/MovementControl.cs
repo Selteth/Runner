fd04ae5eb8edaca7c3c7e45a144d3e7a7c3c2dec
vvok12 <v.v.bezshtanko@gmail.com>
2018-11-10 19:06:17 +0200

    added jump rotation. replaced GroundCheck with vector check

--- Assets/Scripts Original/Player/MovementControl.cs
+++ Assets/Scripts Original/Player/MovementControl.cs
@@ -16,32 +16,49 @@ public class MovementControl : MonoBehaviour
     // Amount of force added to make the player jump
     public float jumpForce = 500f;
     // Position on the player sprite that defines if he is on the ground
-    private Transform groundCheck;
+    ///private Transform groundCheck;
+    
+    // Instead of creating special GameObject on those sides, we need, we should better use a vector math. 
+    // It is becouse our player object will rotate in the jump.
+    public float downVectorLength = 0.27f;
+    
     // Whether or not the player is on the ground
     private bool isGrounded = false;
     // Whether the player should jump
     private bool shouldJump = false;
 
+    [Header("Rotate speed")]
+    //
+    public float angleRotatingImpulse = 10f;
+    // The vector around which the player rotates
+    //private Vector3 rotateCenter = new Vector3(0, 0, 1);
+    private bool isRotating = false;
+    
     // Used new to disable warning about hiding base member
     private new Rigidbody2D rigidbody;
+
+    
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        groundCheck = transform.Find("GroundCheck");
+       // groundCheck = transform.Find("GroundCheck");
     }
     
     void Update()
     {
         horizontalAxis = Input.GetAxis("Horizontal");
         shouldJump = Input.GetButton("Jump");
-        isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        isGrounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y-downVectorLength), 1 << LayerMask.NameToLayer("Ground"));
+        if (isGrounded)
+            isRotating = false;
     }
 
     void FixedUpdate()
     {
         MoveHorizontal();
         MoveVertical();
+        Rotate();
     }
 
     // Handles horizontal movement
@@ -61,7 +78,18 @@ public class MovementControl : MonoBehaviour
         {
             rigidbody.AddForce(Vector2.up * jumpForce);
             isGrounded = false;
+            
         }
     }
-
+    private void Rotate()
+    {
+        if (!isGrounded && !isRotating)
+        {
+            rigidbody.AddForceAtPosition(
+                new Vector2(transform.position.x+1, transform.position.y), 
+                Vector2.left*angleRotatingImpulse*(horizontalAxis>0?(horizontalAxis<0?0:1):-1)
+                );
+            isRotating = true;
+        }
+    }
 }
