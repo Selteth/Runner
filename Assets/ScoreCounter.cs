5a5439c8d470cb92800a7266e4165f4d69dfba4f
vvok12 <v.v.bezshtanko@gmail.com>
2018-12-10 17:03:59 +0200

    score couter + score table

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour {
+
+    public int score=0;
+    public float LengthToScorePoint = 0.1f;
+    private float lengthDone=0f;
+    private float prevPosX = 0;
+	// Use this for initialization
+	void Start () {
+        prevPosX = gameObject.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lengthDone += gameObject.transform.position.x - prevPosX;
+        prevPosX = gameObject.transform.position.x;
+
+        if (lengthDone>LengthToScorePoint)
+        {
+            int delta = Mathf.FloorToInt(lengthDone / LengthToScorePoint);
+            score += delta;
+            lengthDone -= LengthToScorePoint * delta;
+        }
+
+    }
+}
---
+++ Assets/ScoreTableLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTableLogic : MonoBehaviour {
+    private List<int> ScoreTable = new List<int>();
+    public void Add(int res)
+    {
+        ScoreTable.Add(res);
+    }
+
+    public System.Collections.ObjectModel.ReadOnlyCollection<int>GetResults()
+    {
+        ScoreTable.Sort();
+        return ScoreTable.AsReadOnly();
+    }
+}
