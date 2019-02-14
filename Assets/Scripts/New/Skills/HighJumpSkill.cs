5701c36dd28b9264e13cd596dcf6f469650a04db
Selteth <selteth@gmail.com>
2019-02-14 14:05:38 +0200

    Add HighJumpSkill

---
+++ Assets/Scripts/New/Skills/HighJumpSkill.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class HighJumpSkill : SkillBase
+{
+    private enum JumpState
+    {
+        None, Ready, Active, Used
+    }
+
+    private readonly float jumpMultiplier = 2.0f;
+    private Movement playerMovement;
+    private JumpState state;
+
+    void Awake()
+    {
+        playerMovement = GetComponent<Movement>();
+    }
+
+    void FixedUpdate()
+    {
+        if (state == JumpState.None && playerMovement.IsGrounded())
+        {
+            playerMovement.jumpSpeed *= jumpMultiplier;
+            state = JumpState.Ready;
+        }
+        else if (state == JumpState.Ready && !playerMovement.IsGrounded())
+            state = JumpState.Active;
+        else if (state == JumpState.Active && playerMovement.IsGrounded())
+        {
+            playerMovement.jumpSpeed /= jumpMultiplier;
+            state = JumpState.Used;
+        }
+    }
+
+    protected override void DoActivate()
+    {
+        // Empty
+    }
+
+    protected override bool DoDeactivate()
+    {
+        return state == JumpState.Used;
+    }
+}
