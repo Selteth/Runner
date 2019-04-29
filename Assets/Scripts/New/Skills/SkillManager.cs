658ba14af095a5ae2fa3f96d3404724b2f06618c
Selteth <selteth@gmail.com>
2019-04-29 16:22:16 +0300

    Add simple skill ui

---
+++ Assets/Scripts/New/Skills/SkillManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+
+    public void ActivateGhostSkill()
+    {
+        ISkill skill = gameObject.GetComponent<GhostSkill>();
+        if (skill == null)
+            skill = gameObject.AddComponent<GhostSkill>();
+    }
+
+    public void ActivateFlySkill()
+    {
+        ISkill skill = gameObject.GetComponent<FlySkill>();
+        if (skill == null)
+            skill = gameObject.AddComponent<FlySkill>();
+    }
+
+    public void ActivateJumpSkill()
+    {
+        ISkill skill = gameObject.GetComponent<HighJumpSkill>();
+        if (skill == null)
+            skill = gameObject.AddComponent<HighJumpSkill>();
+    }
+
+}
