4ae28412976357dd3fb1e671ef1765a008b0f67b
Selteth <selteth@gmail.com>
2019-04-29 15:12:36 +0300

    Reimport assets and change .NET version to match unity 2019. Fix bug with missing reference to level destroyer script after reloading a level

--- Assets/Scripts/New/Level generation/LevelDestroyer.cs
+++ Assets/Scripts/New/Level generation/LevelDestroyer.cs
@@ -2,19 +2,9 @@
 
 public class LevelDestroyer : MonoBehaviour
 {
-    private float distanceToCamera;
-
     void Start()
     {
-        //Difficulty difficulty = GameObject.Find("Difficulty").GetComponent<Difficulty>();
-        CameraInfo cameraInfo = GetComponentInParent<CameraInfo>();
-
-        distanceToCamera = cameraInfo.GetSize().x * 1.5f + 20f;//difficulty.GetMaxPlatformWidthAll();
-
-        transform.position = new Vector2(
-            transform.parent.position.x - distanceToCamera,
-            transform.parent.position.y
-            );
+        
     }
 
     void OnTriggerEnter2D(Collider2D collider)
